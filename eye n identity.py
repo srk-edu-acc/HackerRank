549c66210f790ab1922ff6baa8f087a89eaf7f22
srk-edu-acc <srkmewid123_bci28@mepcoeng.ac.in>
2026-05-05 10:29:11 +0530

    Create eye n identity.py

---
+++ eye n identity.py
@@ -0,0 +1,10 @@
+import numpy
+N, M = map(int, input().split())
+A = numpy.array([input().split() for _ in range(N)], int)
+B = numpy.array([input().split() for _ in range(N)], int)
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
+print(A ** B)
